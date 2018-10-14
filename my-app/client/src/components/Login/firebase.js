e6490978120a65ae9c2a8f40326fcc816076b5f0
alexanderqchen <aqchen@ucla.edu>
2018-10-13 17:30:33 -0700

    corrected firebase config

--- my-app/client/src/components/Login/firebase.js
+++ my-app/client/src/components/Login/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDblTESEB1SbAVkpy2q39DI2OHphL2-Jxw",
-    authDomain: "fun-food-friends-eeec7.firebaseapp.com",
-    databaseURL: "https://fun-food-friends-eeec7.firebaseio.com",
-    projectId: "fun-food-friends-eeec7",
-    storageBucket: "fun-food-friends-eeec7.appspot.com",
-    messagingSenderId: "144750278413"
-};
+    apiKey: "AIzaSyADIUr4w9VjJ0dfHCNqAjufi0ZSaRVtSvI",
+    authDomain: "sdhacks2018-219306.firebaseapp.com",
+    databaseURL: "https://sdhacks2018-219306.firebaseio.com",
+    projectId: "sdhacks2018-219306",
+    storageBucket: "sdhacks2018-219306.appspot.com",
+    messagingSenderId: "706836162937"
+  };
 firebase.initializeApp(config);
 
 export const provider = new firebase.auth.GoogleAuthProvider();
